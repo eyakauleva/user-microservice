17bce1593657c3fe283cee9df3b0be37e569007d
Elizaveta Yakovleva <eyakovleva@solvd.com>
2023-03-15 16:14:25 +0300

    kafka setup in k8s

---
+++ src/infra/delete.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+kubectl delete deployment.apps/kafka-deploy
+kubectl delete deployment.apps/zookeeper-deploy
+kubectl delete service/kafka-service
+kubectl delete service/zookeeper-service
+
+kubectl delete configmap app-configmap
---
+++ src/infra/execute.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+kubectl apply -f kafka-service.yaml
+kubectl apply -f kafka-deployment.yaml
+
+kubectl apply -f zookeeper-service.yaml
+kubectl apply -f zookeeper-deployment.yaml
+
+kubectl apply -f app-configmap.yaml
+
+#minikube service lawoffice
