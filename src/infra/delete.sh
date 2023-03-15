dbe2334da101d84ecaf08c3f834a2a29363659a5
Elizaveta Yakovleva <eyakovleva@solvd.com>
2023-03-15 17:19:12 +0300

    kafka k8s fix

--- src/infra/delete.sh
+++ src/infra/delete.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 
 kubectl delete deployment.apps/kafka-deploy
-kubectl delete deployment.apps/zookeeper-deploy
 kubectl delete service/kafka-service
+
+kubectl delete deployment.apps/zookeeper-deploy
 kubectl delete service/zookeeper-service
 
-kubectl delete configmap app-configmap
+#kubectl delete configmap app-configmap
