5ee33a720ea153d33e62964daf2dba7d03eaa643
Elizaveta Yakovleva <eyakovleva@solvd.com>
2023-03-17 18:32:42 +0300

    namings fix

--- src/infra/delete.sh
+++ src/infra/delete.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-kubectl delete deployment.apps/kafka-deploy
-kubectl delete service/kafka-service
+#kubectl delete deployment.apps/kafka-deploy
+#kubectl delete service/kafka-service
 
 #kubectl delete deployment.apps/zookeeper-deploy
 #kubectl delete service/zookeeper-service
@@ -10,6 +10,6 @@ kubectl delete deployment.apps/user-deploy
 kubectl delete service/user-service
 kubectl delete configmap user-configmap
 
-#kubectl delete statefulset.apps/postgres-stateful
-#kubectl delete service/postgres-service
-#kubectl delete secret pg-secrets
+#kubectl delete statefulset.apps/pg-stateful-users
+#kubectl delete service/pg-service-users
+#kubectl delete secret pg-secrets-users
--- src/infra/execute.sh
+++ src/infra/execute.sh
@@ -6,9 +6,9 @@
 #kubectl apply -f kafka-service.yaml
 #kubectl apply -f kafka-deployment.yaml
 
-#kubectl apply -f pg-secrets.yaml
-#kubectl apply -f pg-service.yaml
-#kubectl apply -f pg-statefulset.yaml
+kubectl apply -f pg-secrets.yaml
+kubectl apply -f pg-service.yaml
+kubectl apply -f pg-statefulset.yaml
 
 kubectl apply -f app-configmap.yaml
 kubectl apply -f app-service.yaml
