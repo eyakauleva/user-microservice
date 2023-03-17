6555abeb8da6cfc0449cb8551923e4c2597c148b
Elizaveta Yakovleva <eyakovleva@solvd.com>
2023-03-17 17:28:46 +0300

    k8s app & postgres

--- src/infra/delete.sh
+++ src/infra/delete.sh
@@ -3,7 +3,13 @@
 kubectl delete deployment.apps/kafka-deploy
 kubectl delete service/kafka-service
 
-kubectl delete deployment.apps/zookeeper-deploy
-kubectl delete service/zookeeper-service
+#kubectl delete deployment.apps/zookeeper-deploy
+#kubectl delete service/zookeeper-service
 
-#kubectl delete configmap app-configmap
+kubectl delete deployment.apps/user-deploy
+kubectl delete service/user-service
+kubectl delete configmap user-configmap
+
+#kubectl delete statefulset.apps/postgres-stateful
+#kubectl delete service/postgres-service
+#kubectl delete secret pg-secrets
