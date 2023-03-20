9214dcd094e5aea36a06f15b3cee45fe7f09197e
Elizaveta Yakovleva <eyakovleva@solvd.com>
2023-03-20 17:55:29 +0300

    kafka fix

--- src/infra/execute.sh
+++ src/infra/execute.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
-#kubectl apply -f zookeeper-service.yaml
-#kubectl apply -f zookeeper-deployment.yaml
+kubectl apply -f zookeeper-service.yaml
+kubectl apply -f zookeeper-deployment.yaml
 
-#kubectl apply -f kafka-service.yaml
-#kubectl apply -f kafka-deployment.yaml
+kubectl apply -f kafka-service.yaml
+kubectl apply -f kafka-deployment.yaml
 
 kubectl apply -f pg-secrets.yaml
 kubectl apply -f pg-service.yaml
@@ -14,4 +14,4 @@ kubectl apply -f app-configmap.yaml
 kubectl apply -f app-service.yaml
 kubectl apply -f app-deployment.yaml
 
-#minikube service user-service
+minikube service user-service
