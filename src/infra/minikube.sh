631a33f7cadf8129605f90fa78a0cf43ce2822a4
Elizaveta Yakovleva <eyakovleva@solvd.com>
2023-03-25 11:24:30 +0300

    redis on k8s

--- src/infra/minikube.sh
+++ src/infra/minikube.sh
@@ -1,17 +1,21 @@
 #! /bin/bash
 
-#kubectl apply -f zookeeper-service.yaml
-#kubectl apply -f zookeeper-deployment.yaml
-#
-#kubectl apply -f kafka-service.yaml
-#kubectl apply -f kafka-deployment.yaml
-#
-#kubectl apply -f pg-secrets.yaml
-#kubectl apply -f pg-service.yaml
-#kubectl apply -f pg-statefulset.yaml
+kubectl apply -f zookeeper-service.yaml
+kubectl apply -f zookeeper-deployment.yaml
+
+kubectl apply -f kafka-service.yaml
+kubectl apply -f kafka-deployment.yaml
+
+kubectl apply -f pg-secrets.yaml
+kubectl apply -f pg-service.yaml
+kubectl apply -f pg-statefulset.yaml
+
+kubectl apply -f redis-secrets.yaml
+kubectl apply -f redis-service.yaml
+kubectl apply -f redis-statefulset.yaml
 
 kubectl apply -f app-configmap.yaml
 kubectl apply -f app-service.yaml
 kubectl apply -f app-deployment.yaml
 
-#minikube service user-service
+minikube service user-service
