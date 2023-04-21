fa5cb2a1ace2833135d8c01583ee6bdaeba94a9d
Elizaveta Yakovleva <eyakovleva@solvd.com>
2023-04-21 17:22:20 +0300

    pass image tag to k8s deployment file

--- src/infra/minikube.sh
+++ src/infra/minikube.sh
@@ -18,9 +18,10 @@ kubectl apply -f redis-secrets.yaml
 kubectl apply -f redis-service.yaml
 kubectl apply -f redis-statefulset.yaml
 
+export TAG=0.0.4
 kubectl apply -f app-configmap.yaml
 kubectl apply -f app-service.yaml
-kubectl apply -f app-deployment.yaml
+envsubst < app-deployment.yaml | kubectl apply -f -
 
 kubectl apply -f tickets-configmap.yaml
 kubectl apply -f tickets-service.yaml
