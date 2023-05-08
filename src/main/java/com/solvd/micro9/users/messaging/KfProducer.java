30512b6fd01b2ef1aced610fdd2935a3213a4f8c
Elizaveta Yakovleva <eyakovleva@solvd.com>
2023-05-08 21:46:01 +0300

    interface for kafka producer

--- src/main/java/com/solvd/micro9/users/messaging/KfProducer.java
+++ src/main/java/com/solvd/micro9/users/messaging/KfProducer.java
@@ -2,33 +2,11 @@ package com.solvd.micro9.users.messaging;
 
 import com.solvd.micro9.users.domain.aggregate.User;
 import com.solvd.micro9.users.domain.es.Es;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.kafka.core.reactive.ReactiveKafkaProducerTemplate;
-import org.springframework.stereotype.Component;
 
-@Component
-@RequiredArgsConstructor
-public class KfProducer {
+public interface KfProducer {
 
-    @Value("${spring.kafka.producer.topic}")
-    private String topic;
+    void send(String key, Es value);
 
-    @Value("${spring.kafka.producer.syncTopic}")
-    private String syncTopic;
-
-    private final ReactiveKafkaProducerTemplate<String, Es> producer;
-
-    private final ReactiveKafkaProducerTemplate<String, User> syncProducer;
-
-    public void send(final String key, final Es value) {
-        producer.send(topic, key, value)
-                .subscribe();
-    }
-
-    public void send(final String key, final User value) {
-        syncProducer.send(syncTopic, key, value)
-                .subscribe();
-    }
+    void send(String key, User value);
 
 }
