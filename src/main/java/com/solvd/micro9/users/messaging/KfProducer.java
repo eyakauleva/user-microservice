d79e11519f5e9f609a899f8f162777d96e013a71
Elizaveta Yakovleva <eyakovleva@solvd.com>
2023-05-08 19:30:27 +0300

    send data to kafka

--- src/main/java/com/solvd/micro9/users/messaging/KfProducer.java
+++ src/main/java/com/solvd/micro9/users/messaging/KfProducer.java
@@ -1,5 +1,6 @@
 package com.solvd.micro9.users.messaging;
 
+import com.solvd.micro9.users.domain.aggregate.User;
 import com.solvd.micro9.users.domain.es.Es;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,10 +14,20 @@ public class KfProducer {
     @Value("${spring.kafka.producer.topic}")
     private String topic;
 
+    @Value("${spring.kafka.producer.syncTopic}")
+    private String syncTopic;
+
     private final ReactiveKafkaProducerTemplate<String, Es> producer;
+
+    private final ReactiveKafkaProducerTemplate<String, User> syncProducer;
 
     public void send(final String key, final Es value) {
         producer.send(topic, key, value)
+                .subscribe();
+    }
+
+    public void send(final String key, final User value) {
+        syncProducer.send(syncTopic, key, value)
                 .subscribe();
     }
 
