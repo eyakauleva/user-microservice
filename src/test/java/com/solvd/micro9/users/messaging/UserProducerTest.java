e41bb0c9b9ff759cf25434e9b3c01db50b53e7a3
Elizaveta Yakovleva <eyakovleva@solvd.com>
2023-05-10 16:46:48 +0300

    trying to fix github aciton

--- src/test/java/com/solvd/micro9/users/messaging/UserProducerTest.java
+++ src/test/java/com/solvd/micro9/users/messaging/UserProducerTest.java
@@ -14,6 +14,7 @@ import org.springframework.kafka.test.EmbeddedKafkaBroker;
 import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.kafka.test.utils.KafkaTestUtils;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.TestPropertySource;
 
 import java.time.Duration;
 import java.util.Collections;
@@ -23,6 +24,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Slf4j
 @SpringBootTest
+@TestPropertySource(properties = {"ticket-service = localhost:9090"})
 @DirtiesContext
 @EmbeddedKafka(partitions = 1, brokerProperties = {"listeners=PLAINTEXT://localhost:9092", "port=9092"})
 public class UserProducerTest {
