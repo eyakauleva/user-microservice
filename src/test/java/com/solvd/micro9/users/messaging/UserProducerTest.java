d918db30ac54a1eae5e37a3d986b2e3a18c5c726
Elizaveta Yakovleva <eyakovleva@solvd.com>
2023-05-11 19:28:55 +0300

    moved from embeddedKafka to testcontainers

--- src/test/java/com/solvd/micro9/users/messaging/UserProducerTest.java
+++ src/test/java/com/solvd/micro9/users/messaging/UserProducerTest.java
@@ -3,21 +3,25 @@ package com.solvd.micro9.users.messaging;
 import com.google.gson.Gson;
 import com.solvd.micro9.users.domain.aggregate.User;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.consumer.Consumer;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.kafka.test.EmbeddedKafkaBroker;
-import org.springframework.kafka.test.context.EmbeddedKafka;
-import org.springframework.kafka.test.utils.KafkaTestUtils;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.TestPropertySource;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 import java.time.Duration;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -26,13 +30,21 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest
 @TestPropertySource(properties = {"ticket-service = localhost:9090"})
 @DirtiesContext
-@EmbeddedKafka(partitions = 1, brokerProperties = {"listeners=PLAINTEXT://localhost:9092", "port=9092"})
+@Testcontainers
 public class UserProducerTest {
 
     private final static String TOPIC = "syncMongoElastic";
 
-    @Autowired
-    private EmbeddedKafkaBroker embeddedKafka;
+    @Container
+    public static KafkaContainer kafkaContainer = new KafkaContainer(
+            DockerImageName.parse("confluentinc/cp-kafka:7.3.0")
+    );
+
+    @DynamicPropertySource
+    static void kafkaProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.kafka.bootstrap-servers", kafkaContainer::getBootstrapServers);
+        registry.add("spring.kafka.producer.value-serializer", () -> JsonSerializer.class);
+    }
 
     @Autowired
     private UserProducer producer;
@@ -40,10 +52,16 @@ public class UserProducerTest {
     @Test
     public void verifyMessageSentToKafkaTest() {
         //given
-        User user = new User("1111", "Liza", "Ya", "email@gmail.com", false);
+        User user = new User("9999", "Liza", "Ya", "email@gmail.com", false);
 
-        Map<String, Object> consumerProps = KafkaTestUtils.consumerProps("test-group", "true", embeddedKafka);
-        try (Consumer<String, User> consumer = new KafkaConsumer<>(consumerProps)) {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaContainer.getBootstrapServers());
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "test-group");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(JsonDeserializer.VALUE_DEFAULT_TYPE, com.solvd.micro9.users.domain.aggregate.User.class);
+
+        try (Consumer<String, User> consumer = new KafkaConsumer<>(props)) {
             consumer.subscribe(Collections.singleton(TOPIC));
 
             //when
@@ -61,5 +79,6 @@ public class UserProducerTest {
             assertEquals(user, result);
         }
     }
+
 
 }
