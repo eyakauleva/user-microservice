f6768c510665c8275144b0e26e9ced558a3c98e9
Elizaveta Yakovleva <eyakovleva@solvd.com>
2023-05-12 13:22:16 +0300

    testcontainers config class; fixes

---
+++ src/test/java/com/solvd/micro9/users/messaging/TestcontainersTest.java
@@ -0,0 +1,54 @@
+package com.solvd.micro9.users.messaging;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Testcontainers
+public abstract class TestcontainersTest {
+
+    private final int pollSeconds = 5;
+
+    @Container
+    private static final KafkaContainer KAFKA_CONTAINER = new KafkaContainer(
+            DockerImageName.parse("confluentinc/cp-kafka:7.3.0")
+    );
+
+    @DynamicPropertySource
+    static void kafkaProperties(final DynamicPropertyRegistry registry) {
+        registry.add("ticket-service", () -> "localhost:9090");
+        registry.add("spring.kafka.bootstrap-servers",
+                KAFKA_CONTAINER::getBootstrapServers
+        );
+    }
+
+    public Map<String, Object> getConsumerProps(final Class<?> clazz) {
+        Map<String, Object> props = new HashMap<>();
+        props.put(
+                ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                KAFKA_CONTAINER.getBootstrapServers()
+        );
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "test-group");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(
+                ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+                StringDeserializer.class
+        );
+        props.put(JsonDeserializer.VALUE_DEFAULT_TYPE, clazz);
+        return props;
+    }
+
+    public int getPollSeconds() {
+        return pollSeconds;
+    }
+
+}
---
+++ src/test/java/com/solvd/micro9/users/messaging/UserProducerIT.java
@@ -0,0 +1,49 @@
+package com.solvd.micro9.users.messaging;
+
+import com.google.gson.Gson;
+import com.solvd.micro9.users.domain.aggregate.User;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+import java.time.Duration;
+import java.util.Collections;
+
+@Slf4j
+@SpringBootTest
+@DirtiesContext
+public class UserProducerIT extends TestcontainersTest {
+
+    private static final String TOPIC = "syncMongoElastic";
+
+    @Autowired
+    private UserProducer producer;
+
+    @Test
+    public void verifyMessageSentToKafkaTest() {
+        User user = new User("9999", "Liza", "Ya", "email@gmail.com", false);
+        try (Consumer<String, User> consumer = new KafkaConsumer<>(
+                getConsumerProps(User.class)
+        )) {
+            consumer.subscribe(Collections.singleton(TOPIC));
+            producer.send(user.getId(), user);
+            ConsumerRecords<String, User> records = consumer.poll(
+                    Duration.ofSeconds(getPollSeconds())
+            );
+            ConsumerRecord<String, User> record = records.iterator().next();
+            User result = new Gson().fromJson(
+                    String.valueOf(record.value()), User.class
+            );
+            Assertions.assertEquals(1, records.count());
+            Assertions.assertEquals(user, result);
+        }
+    }
+
+}
