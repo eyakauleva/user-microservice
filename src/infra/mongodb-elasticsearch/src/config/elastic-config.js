4d6d647724af651e47f74db247dfa3a538f787ca
Elizaveta Yakovleva <eyakovleva@solvd.com>
2023-05-22 15:47:13 +0300

    ELASTIC_DOMAIN env renamed

--- src/infra/mongodb-elasticsearch/src/config/elastic-config.js
+++ src/infra/mongodb-elasticsearch/src/config/elastic-config.js
@@ -2,7 +2,7 @@ const {Client} = require('@elastic/elasticsearch')
 require("dotenv").config();
 
 const client = new Client({
-    node: 'http://' + process.env.ELASTIC_DOMAIN + ':9200',
+    node: 'http://' + process.env.ELASTIC_HOST + ':9200',
     auth: {
         username: process.env.ELASTIC_USER,
         password: process.env.ELASTIC_PASSWORD
